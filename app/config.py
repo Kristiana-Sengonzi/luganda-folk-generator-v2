8388613a4b19272db022e10406b4a70f4576abbd
Kristiana-Sengonzi <kristianasengonzi@gmail.com>
2025-11-01 20:02:33 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ BASE_MODEL_PATH = "/root/models/base_model"  # Same as before
 STORY_GENERATOR_PATH = "/root/models/story_lora"  # Same as before
 LYRIC_GENERATOR_PATH = "/root/models/lyric_lora"  # Same as before
 TRANSLATOR_PATH = "/root/models/translator"  # Same as before
-AUDIO_VAE_PATH = "/root/models/audio_vae"  # Same as before
+AUDIO_VAE_PATH = "/root/models/audio_vae/full_model.pth"  # Same as before
 HARTMANN_EMOTION_PATH = "/root/models/emotion"  # Same as before
 
 API_KEY = "test-key-12345"
